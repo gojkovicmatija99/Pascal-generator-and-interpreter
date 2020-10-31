fac439b979a0bd626ee85bb7328713242ecf1987
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2020-10-31 01:00:28 +0100

    Started working on grapher.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from enum import Enum, auto
 from functools import wraps
 import pickle
 import sys
+from graphviz import Digraph, Source
+from IPython.display import Image
 
 
 class Class(Enum):
@@ -849,21 +851,200 @@ class Parser:
     def die_type(self, expected, found):
         self.die("Expected: {}, Found: {}".format(expected, found))
 
-try:
-    i = 0
-    for i in range(12):
-        test_id = i+1
-        path = f'.\\test\\test{test_id}.pas'
 
-        with open(path, 'r') as source:
-            text = source.read()
+class Visitor():
+    def visit(self, parent, node):
+        method = 'visit_' + type(node).__name__
+        visitor = getattr(self, method, self.die)
+        return visitor(parent, node)
 
-            lexer = Lexer(text)
-            tokens = lexer.lex()
+    def die(self, parent, node):
+        method = 'visit_' + type(node).__name__
+        raise SystemExit("Missing method: {}".format(method))
 
-            parser = Parser(tokens)
-            ast = parser.parse()
 
-            print(ast)
-except:
-    print(f'error in test{i+1}.pas')
+class Grapher(Visitor):
+    def __init__(self, ast):
+        self.ast = ast
+        self._count = 1
+        self.dot = Digraph()
+        self.dot.node_attr['shape'] = 'box'
+        self.dot.node_attr['height'] = '0.1'
+        self.dot.edge_attr['arrowsize'] = '0.5'
+
+    def add_node(self, parent, node, name=None):
+        node._index = self._count
+        self._count += 1
+        caption = type(node).__name__
+        if name is not None:
+            caption = '{} : {}'.format(caption, name)
+        self.dot.node('node{}'.format(node._index), caption)
+        if parent is not None:
+            self.add_edge(parent, node)
+
+    def add_edge(self, parent, node):
+        src, dest = parent._index, node._index
+        self.dot.edge('node{}'.format(src), 'node{}'.format(dest))
+
+    def visit_Program(self, parent, node):
+        self.add_node(parent, node)
+        for n in node.nodes:
+            self.visit(node, n)
+
+    def visit_Decl(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.type_)
+        self.visit(node, node.id_)
+
+    def visit_Variables(self, parent, node):
+        self.add_node(parent, node)
+        for n in node.nodes:
+            self.visit(node, n)
+
+    def visit_Main(self, parent, node):
+        self.add_node(parent, node)
+        if node.variables is not None:
+            self.visit(node, node.variables)
+        self.visit(node, node.block)
+
+    def visit_Procedure(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.id_)
+        self.visit(node, node.params)
+        #self.visit(node, node.variables)
+        self.visit(node, node.block)
+
+    def visit_ArrayDecl(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.type_)
+        self.visit(node, node.id_)
+        if node.size is not None:
+            self.visit(node, node.size)
+        if node.elems is not None:
+            self.visit(node, node.elems)
+
+    def visit_ArrayElem(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.id_)
+        self.visit(node, node.index)
+
+    def visit_Assign(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.id_)
+        self.visit(node, node.expr)
+
+    def visit_If(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.cond)
+        self.visit(node, node.true)
+        if node.false is not None:
+            self.visit(node, node.false)
+
+    def visit_While(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.cond)
+        self.visit(node, node.block)
+
+    def visit_For(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.init)
+        self.visit(node, node.cond)
+        self.visit(node, node.step)
+        self.visit(node, node.block)
+
+    def visit_FuncImpl(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.type_)
+        self.visit(node, node.id_)
+        self.visit(node, node.params)
+        self.visit(node, node.block)
+
+    def visit_FuncCall(self, parent, node):
+        self.add_node(parent, node)
+        self.visit(node, node.id_)
+        self.visit(node, node.args)
+
+    def visit_Block(self, parent, node):
+        self.add_node(parent, node)
+        for n in node.nodes:
+            self.visit(node, n)
+
+    def visit_Params(self, parent, node):
+        self.add_node(parent, node)
+        for p in node.params:
+            self.visit(node, p)
+
+    def visit_Args(self, parent, node):
+        self.add_node(parent, node)
+        for a in node.args:
+            self.visit(node, a)
+
+    def visit_Elems(self, parent, node):
+        self.add_node(parent, node)
+        for e in node.elems:
+            self.visit(node, e)
+
+    def visit_Break(self, parent, node):
+        self.add_node(parent, node)
+
+    def visit_Continue(self, parent, node):
+        self.add_node(parent, node)
+
+    def visit_Return(self, parent, node):
+        self.add_node(parent, node)
+        if node.expr is not None:
+            self.visit(node, node.expr)
+
+    def visit_Type(self, parent, node):
+        name = node.value
+        self.add_node(parent, node, name)
+
+    def visit_Int(self, parent, node):
+        name = node.value
+        self.add_node(parent, node, name)
+
+    def visit_Char(self, parent, node):
+        name = node.value
+        self.add_node(parent, node, name)
+
+    def visit_String(self, parent, node):
+        name = node.value
+        self.add_node(parent, node, name)
+
+    def visit_Id(self, parent, node):
+        name = node.value
+        self.add_node(parent, node, name)
+
+    def visit_BinOp(self, parent, node):
+        name = node.symbol
+        self.add_node(parent, node, name)
+        self.visit(node, node.first)
+        self.visit(node, node.second)
+
+    def visit_UnOp(self, parent, node):
+        name = node.symbol
+        self.add_node(parent, node, name)
+        self.visit(node, node.first)
+
+    def graph(self):
+        self.visit(None, self.ast)
+        s = Source(self.dot.source, filename='graph', format='png')
+        return s.view()
+
+
+test_id = 1
+path = f'./test/test{test_id}.pas'
+
+with open(path, 'r') as source:
+    text = source.read()
+
+    lexer = Lexer(text)
+    tokens = lexer.lex()
+
+    parser = Parser(tokens)
+    ast = parser.parse()
+
+    grapher = Grapher(ast)
+    img = grapher.graph()
+
+    print(ast)
