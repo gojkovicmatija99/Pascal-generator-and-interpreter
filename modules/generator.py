25c50e0a9343ef99a860629919880231b37dc991
Matija Gojkovic <59018092+gojkovicmatija99@users.noreply.github.com>
2020-12-02 00:37:22 +0100

    BinOp needs fixing for test2.pas

--- modules/generator.py
+++ modules/generator.py
@@ -31,23 +31,23 @@ class Generator(Visitor):
         self.newline()
 
     def visit_Program(self, parent, node):
-        is_in_main = False
+        is_in_main = True
         for n in node.nodes:
-            if is_in_main and type(n) is Var or type(n) is Block:
+            if is_in_main and (type(n) is Var or type(n) is Block):
                 self.append("int main()")
                 self.open_scope()
-                is_in_main = True
+                is_in_main = False
             self.visit(node, n)
         self.indent()
         self.append("return 0;")
         self.newline()
         self.close_scope()
 
-
     def visit_Block(self, parent, node):
         for n in node.nodes:
             self.indent()
             self.visit(node, n)
+            self.append(";")
             self.newline()
 
     def visit_FuncProcCall(self, parent, node):
@@ -56,7 +56,7 @@ class Generator(Visitor):
         if func == 'writeln':
             self.append('printf(')
             self.visit(node, node.args)
-            self.append(');')
+            self.append(')')
 
     def visit_Args(self, parent, node):
         for i, a in enumerate(node.args):
@@ -68,6 +68,45 @@ class Generator(Visitor):
         self.append('"')
         self.append(node.value)
         self.append('"')
+
+    def visit_Char(self, parent, node):
+        self.append("'")
+        self.append(node.value)
+        self.append("'")
+
+    def visit_Int(self, parent, node):
+        self.append(node.value)
+
+    def visit_BinOp(self, parent, node):
+        #self.visit(node.first)
+        self.append(node.symbol)
+        #self.visit(node.second)
+
+    def visit_Var(self, parent, node):
+        for n in node.nodes:
+            self.visit(node, n)
+
+    def visit_Decl(self, parent, node):
+        self.indent()
+        self.visit(node, node.type_)
+        self.append(" ")
+        self.visit(node, node.id_)
+        self.append(";")
+        self.newline()
+
+    def visit_Assign(self, parent, node):
+        self.visit(node, node.id_)
+        self.append(" = ")
+        self.visit(node, node.expr)
+
+    def visit_Type(self, parent, node):
+        if node.value == "integer":
+            self.append("int")
+        else:
+            self.append(node.value)
+
+    def visit_Id(self, parent, node):
+        self.append(node.value)
 
     def generate(self, path):
         self.visit(None, self.ast)
